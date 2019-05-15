d2b3554ab26ea3484e51303e62a306440ea2b98e
Miguel <miguel.alvarez@enti.cat>
2019-05-15 12:57:43 +0200

    carga las cosas, yupi

--- glframework/code/src/render.cpp
+++ glframework/code/src/render.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <vector>
 
 #include <imgui\imgui.h>
@@ -128,45 +130,46 @@ void linkProgram(GLuint program) {
 bool loadOBJ(const char * path,
 	std::vector < glm::vec3 > & out_vertices,
 	std::vector < glm::vec2 > & out_uvs,
-	std::vector < glm::vec3 > & out_normals)
-{
+	std::vector < glm::vec3 > & out_normals
+) {
 	std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
 	std::vector< glm::vec3 > temp_vertices;
 	std::vector< glm::vec2 > temp_uvs;
 	std::vector< glm::vec3 > temp_normals;
 
-	FILE * file;
-	fopen_s(&file, path, "r");
+	FILE * file = fopen(path, "r");
 	if (file == NULL) {
 		printf("Impossible to open the file !\n");
 		return false;
 	}
+
 	while (1) {
+
 		char lineHeader[128];
 		// read the first word of the line
-		int res = fscanf_s(file, "%s", lineHeader, 128);
+		int res = fscanf(file, "%s", lineHeader);
 		if (res == EOF)
 			break; // EOF = End Of File. Quit the loop.
 
 		if (strcmp(lineHeader, "v") == 0) {
 			glm::vec3 vertex;
-			fscanf_s(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
 			temp_vertices.push_back(vertex);
 		}
 		else if (strcmp(lineHeader, "vt") == 0) {
 			glm::vec2 uv;
-			fscanf_s(file, "%f %f\n", &uv.x, &uv.y);
+			fscanf(file, "%f %f\n", &uv.x, &uv.y);
 			temp_uvs.push_back(uv);
 		}
 		else if (strcmp(lineHeader, "vn") == 0) {
 			glm::vec3 normal;
-			fscanf_s(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
 			temp_normals.push_back(normal);
 		}
 		else if (strcmp(lineHeader, "f") == 0) {
 			std::string vertex1, vertex2, vertex3;
 			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-			int matches = fscanf_s(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
 			if (matches != 9) {
 				printf("File can't be read by our simple parser : ( Try exporting with other options\n");
 				return false;
@@ -181,25 +184,31 @@ bool loadOBJ(const char * path,
 			normalIndices.push_back(normalIndex[1]);
 			normalIndices.push_back(normalIndex[2]);
 		}
+
+
+
+
+
+
 	}
+
 	// For each vertex of each triangle
+
 	for (unsigned int i = 0; i < vertexIndices.size(); i++) {
 		unsigned int vertexIndex = vertexIndices[i];
 		glm::vec3 vertex = temp_vertices[vertexIndex - 1];
 		out_vertices.push_back(vertex);
-	}
-
-	for (unsigned int i = 0; i < uvIndices.size(); i++) {
-		unsigned int uvIndex = uvIndices[i];
-		glm::vec2 vertex = temp_uvs[uvIndex - 1];
-		out_uvs.push_back(vertex);
-	}
-
-	for (unsigned int i = 0; i < normalIndices.size(); i++) {
+		unsigned int uvindex = uvIndices[i];
+		glm::vec2 uv = temp_uvs[uvindex - 1];
+		out_uvs.push_back(uv);
 		unsigned int normalIndex = normalIndices[i];
-		glm::vec3 vertex = temp_normals[normalIndex - 1];
-		out_normals.push_back(vertex);
+		glm::vec3 normal = temp_normals[normalIndex - 1];
+		out_normals.push_back(normal);
 	}
+
+
+
+	return true;
 }
 
 ////////////////////////////////////////////////// AXIS
@@ -506,7 +515,7 @@ void main() {\n\
 	void setupObject() {
 		std::vector<glm::vec3> verts, norms;
 		std::vector<glm::vec2> uvs;
-		loadOBJ("Cabin.obj", verts, uvs, norms);
+		loadOBJ("Support.obj", verts, uvs, norms);
 
 		k_amb = k_dif = .5f;
 		k_spe = 1.f;
